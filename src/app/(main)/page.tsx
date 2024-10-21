03e08de8dc0922900ee77abf182191ae047c39af
NAUJVZLA <ju4nc4r1d4d@gmail.com>
2024-10-21 09:35:46 -0500

    update

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -122,7 +122,7 @@ export default function Home() {
         </section>
 
         {/* Proyectos Destacados */}
-        <section className="py-16 bg-gray-100 ">
+        <section className="py-16 ">
           <div className="container mx-auto px-4">
             <h2 className="text-3xl font-bold mb-8 text-center">Proyectos Destacados</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
